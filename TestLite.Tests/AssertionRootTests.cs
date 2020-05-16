cbd9cb4dcdd5642417f0c2103edf764fa3f40ad2
Balázs Kis <19716399+balazs-kis@users.noreply.github.com>
2020-05-16 17:42:19 +0200

    Added new AssertionRoot tests

--- TestLite.Tests/AssertionRootTests.cs
+++ TestLite.Tests/AssertionRootTests.cs
@@ -26,6 +26,25 @@ namespace TestLite.Tests
         }
 
         [TestMethod]
+        public void GenericOkResult_IsSuccessDoesNotThrowException()
+        {
+            var result = ActResult.Ok(0);
+            var underTest = new AssertionRoot(result);
+            Exception exception = null;
+
+            try
+            {
+                underTest.IsSuccess();
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+
+            Assert.IsNull(exception);
+        }
+
+        [TestMethod]
         public void ErrorResult_IsSuccessThrowsException()
         {
             var result = ActResult.ThrewException(new Exception());
@@ -43,6 +62,66 @@ namespace TestLite.Tests
 
             Assert.IsNotNull(exception);
             Assert.IsInstanceOfType(exception, typeof(AssertFailedException));
+        }
+
+        [TestMethod]
+        public void ErrorResult_IsSuccessThrowsExceptionWithMessage()
+        {
+            const string message = "Should work";
+            var result = ActResult.ThrewException(new Exception());
+            var underTest = new AssertionRoot(result);
+            Exception exception = null;
+
+            try
+            {
+                underTest.IsSuccess(message);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+
+            Assert.IsTrue(exception.Message.Contains(message));
+        }
+
+        [TestMethod]
+        public void GenericErrorResult_IsSuccessThrowsException()
+        {
+            var result = ActResult.ThrewException<int>(new Exception());
+            var underTest = new AssertionRoot(result);
+            Exception exception = null;
+
+            try
+            {
+                underTest.IsSuccess();
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+
+            Assert.IsNotNull(exception);
+            Assert.IsInstanceOfType(exception, typeof(AssertFailedException));
+        }
+
+        [TestMethod]
+        public void GenericErrorResult_IsSuccessThrowsExceptionWithMessage()
+        {
+            const string message = "Should work";
+            var result = ActResult.ThrewException<int>(new Exception());
+            var underTest = new AssertionRoot(result);
+            Exception exception = null;
+
+            try
+            {
+                underTest.IsSuccess(message);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+
+            Assert.IsTrue(exception.Message.Contains(message));
         }
     }
 }
