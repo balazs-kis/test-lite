3a4c2f99e6e8daa41fce9aba77c06e4d011a63ae
Balázs Kis <19716399+balazs-kis@users.noreply.github.com>
2020-05-16 19:31:42 +0200

    Static test starter tests added

---
+++ TestLite.Tests/StaticTestStarterTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestLite.Tests
+{
+    [TestClass]
+    public class StaticTestStarterTests
+    {
+        [TestMethod]
+        public void NoArrangement_ReturnsNewTypelessArrangedClass()
+        {
+            var result = Test.Arrange();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Arranged));
+        }
+
+        [TestMethod]
+        public void ArrangeWithFunc_ReturnsGenericArrangedClass()
+        {
+            var called = false;
+            var result = Test.Arrange(() =>
+            {
+                called = true;
+                return 1;
+            });
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Arranged<int>));
+            Assert.IsTrue(called);
+        }
+
+        [TestMethod]
+        public void ArrangeWithFunc_ReturnsGenericArrangedClassWithOneParameter()
+        {
+            var called = false;
+            var result = Test.Arrange(() =>
+            {
+                called = true;
+                return (1, true);
+            });
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Arranged<int, bool>));
+            Assert.IsTrue(called);
+        }
+
+        [TestMethod]
+        public void ArrangeWithFunc_ReturnsGenericArrangedClassWithTwoParameters()
+        {
+            var called = false;
+            var result = Test.Arrange(() =>
+            {
+                called = true;
+                return (1, true, 0.5);
+            });
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Arranged<int, bool, double>));
+            Assert.IsTrue(called);
+        }
+    }
+}
--- TestLite/Test.cs
+++ TestLite/Test.cs
@@ -17,7 +17,7 @@ namespace TestLite
         }
 
         /// <summary>
-        /// Define the <b>arrange</b>section of the test as a function
+        /// Define the <b>arrange</b> section of the test as a function
         /// which returns the unit under test
         /// </summary>
         public static Arranged<T> Arrange<T>(Func<T> arrangeFunc)
@@ -27,7 +27,7 @@ namespace TestLite
         }
 
         /// <summary>
-        /// Define the <b>arrange</b>section of the test as a function
+        /// Define the <b>arrange</b> section of the test as a function
         /// which returns the unit under test and an additional paramater
         /// </summary>
         public static Arranged<T, TParameter> Arrange<T, TParameter>(Func<(T, TParameter)> arrangeFunc)
@@ -37,7 +37,7 @@ namespace TestLite
         }
 
         /// <summary>
-        /// Define the <b>arrange</b>section of the test as a function
+        /// Define the <b>arrange</b> section of the test as a function
         /// which returns the unit under test and two additional paramaters
         /// </summary>
         public static Arranged<T, TParameter1, TParameter2> Arrange<T, TParameter1, TParameter2>(
