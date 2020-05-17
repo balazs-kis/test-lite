8ae933c5c85d53214f36c3651efdc76f577d4ee9
Balázs Kis <19716399+balazs-kis@users.noreply.github.com>
2020-05-17 17:59:57 +0200

    Tests for Arranged class (part 1)

---
+++ TestLite.Tests/ArrangedTests.cs
@@ -0,0 +1,209 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace TestLite.Tests
+{
+    [TestClass]
+    public class ArrangedTests
+    {
+        [TestMethod]
+        public void Act_ActActionWithoutException_ActedInstanceReturned()
+        {
+            var arranged = new Arranged();
+
+            var result = arranged.Act(() => { });
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Acted));
+        }
+
+        [TestMethod]
+        public void Act_ActActionWithoutException_ActActionIsCalled()
+        {
+            var arranged = new Arranged();
+            var called = false;
+
+            arranged.Act(() => called = true);
+
+            Assert.IsTrue(called);
+        }
+
+        [TestMethod]
+        public void Act_ActActionWithException_ActedInstanceReturned()
+        {
+            var arranged = new Arranged();
+
+            var result = arranged.Act(() => throw new IOException());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Acted));
+        }
+
+        [TestMethod]
+        public void Act_ActActionWithException_ActActionIsCalled()
+        {
+            var arranged = new Arranged();
+            var called = false;
+
+            arranged.Act(() =>
+            {
+                called = true;
+                throw new IOException();
+            });
+
+            Assert.IsTrue(called);
+        }
+
+        [TestMethod]
+        public void ActWithReturnType_ActActionWithoutException_ActedInstanceReturned()
+        {
+            var arranged = new Arranged();
+
+            var result = arranged.Act(() => 1);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Acted<int>));
+        }
+
+        [TestMethod]
+        public void ActWithReturnType_ActActionWithoutException_ActActionIsCalled()
+        {
+            var arranged = new Arranged();
+            var called = false;
+
+            arranged.Act(() =>
+            {
+                called = true;
+                return 1;
+            });
+
+            Assert.IsTrue(called);
+        }
+
+        [TestMethod]
+        public void ActWithReturnType_ActActionWithException_ActedInstanceReturned()
+        {
+            var arranged = new Arranged();
+
+            var result = arranged.Act<int>(() => throw new IOException());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Acted<int>));
+        }
+
+        [TestMethod]
+        public void ActWithReturnType_ActActionWithException_ActActionIsCalled()
+        {
+            var arranged = new Arranged();
+            var called = false;
+
+            arranged.Act<int>(() =>
+            {
+                called = true;
+                throw new IOException();
+            });
+
+            Assert.IsTrue(called);
+        }
+
+        [TestMethod]
+        public void ActWithInput_ActActionWithoutException_ActedInstanceReturned()
+        {
+            var arranged = new Arranged<string>("input");
+
+            var result = arranged.Act(input => { });
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Acted));
+        }
+
+        [TestMethod]
+        public void ActWithInput_ActActionWithoutException_ActActionIsCalled()
+        {
+            var arranged = new Arranged<string>("input");
+            var called = false;
+
+            arranged.Act(input => called = true);
+
+            Assert.IsTrue(called);
+        }
+
+        [TestMethod]
+        public void ActWithInput_ActActionWithException_ActedInstanceReturned()
+        {
+            var arranged = new Arranged<string>("input");
+
+            var result = arranged.Act(input => throw new IOException());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Acted));
+        }
+
+        [TestMethod]
+        public void ActWithInput_ActActionWithException_ActActionIsCalled()
+        {
+            var arranged = new Arranged<string>("input");
+            var called = false;
+
+            arranged.Act(input =>
+            {
+                called = true;
+                throw new IOException();
+            });
+
+            Assert.IsTrue(called);
+        }
+
+        [TestMethod]
+        public void ActWithInputAndReturnType_ActActionWithoutException_ActedInstanceReturned()
+        {
+            var arranged = new Arranged<string>("input");
+
+            var result = arranged.Act(input => 1);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Acted<int>));
+        }
+
+        [TestMethod]
+        public void ActWithInputAndReturnType_ActActionWithoutException_ActActionIsCalled()
+        {
+            var arranged = new Arranged<string>("input");
+            var called = false;
+
+            arranged.Act(input =>
+            {
+                called = true;
+                return 1;
+            });
+
+            Assert.IsTrue(called);
+        }
+
+        [TestMethod]
+        public void ActWithInputAndReturnType_ActActionWithException_ActedInstanceReturned()
+        {
+            var arranged = new Arranged<string>("input");
+
+            var result = arranged.Act<int>(input => throw new IOException());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Acted<int>));
+        }
+
+        [TestMethod]
+        public void ActWithInputAndReturnType_ActActionWithException_ActActionIsCalled()
+        {
+            var arranged = new Arranged<string>("input");
+            var called = false;
+
+            arranged.Act<int>(input =>
+            {
+                called = true;
+                throw new IOException();
+            });
+
+            Assert.IsTrue(called);
+        }
+    }
+}
