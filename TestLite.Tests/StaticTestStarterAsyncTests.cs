60f6e9e3211994953cad644cb704149b0784cb45
Balázs Kis <19716399+balazs-kis@users.noreply.github.com>
2020-06-22 13:51:02 +0200

    Improve coverage (part 1)

---
+++ TestLite.Tests/StaticTestStarterAsyncTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestLite.Tests
+{
+    [TestClass]
+    public class StaticTestStarterAsyncTests
+    {
+        [TestMethod]
+        public void ArrangeAsyncWithFunc_ReturnsGenericArrangedClass()
+        {
+            var called = false;
+            var result = Test.ArrangeAsync(async () =>
+            {
+                await DoSomethingAsync();
+                called = true;
+                return 1;
+            });
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Arranged<int>));
+            Assert.IsTrue(called);
+        }
+
+        [TestMethod]
+        public void ArrangeAsyncWithFunc_ReturnsGenericArrangedClassWithOneParameter()
+        {
+            var called = false;
+            var result = Test.ArrangeAsync(async () =>
+            {
+                await DoSomethingAsync();
+                called = true;
+                return (1, true);
+            });
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Arranged<int, bool>));
+            Assert.IsTrue(called);
+        }
+
+        [TestMethod]
+        public void ArrangeAsyncWithFunc_ReturnsGenericArrangedClassWithTwoParameters()
+        {
+            var called = false;
+            var result = Test.ArrangeAsync(async () =>
+            {
+                await DoSomethingAsync();
+                called = true;
+                return (1, true, 0.5);
+            });
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Arranged<int, bool, double>));
+            Assert.IsTrue(called);
+        }
+
+
+        private static Task DoSomethingAsync()
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
