c8393730dfe7238927f0bcf50d2d9e18099d0db4
Balázs Kis <19716399+balazs-kis@users.noreply.github.com>
2020-06-22 10:22:25 +0200

    Async 'Arrange' and 'Act' versions added

--- TestLite/Arranged.cs
+++ TestLite/Arranged.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace TestLite
 {
@@ -11,13 +12,27 @@ namespace TestLite
             new Acted(CatchException(actAction));
 
         /// <summary>
+        /// Define the <b>act</b>section of the test as an action
+        /// </summary>
+        public Acted ActAsync(Func<Task> actAction) =>
+            new Acted(CatchExceptionAsync(actAction));
+
+        /// <summary>
         /// Define the <b>act</b>section of the test as a function
         /// which returns the result of the test
         /// </summary>
         public Acted<TResult> Act<TResult>(Func<TResult> actFunc) =>
             new Acted<TResult>(CatchException(actFunc));
 
-        private ActResult CatchException(Action action)
+        /// <summary>
+        /// Define the <b>act</b>section of the test as a function
+        /// which returns the result of the test
+        /// </summary>
+        public Acted<TResult> ActAsync<TResult>(Func<Task<TResult>> actFunc) =>
+            new Acted<TResult>(CatchExceptionAsync(actFunc));
+
+
+        private static ActResult CatchException(Action action)
         {
             try
             {
@@ -30,13 +45,42 @@ namespace TestLite
             }
         }
 
-        private ActResult<T> CatchException<T>(Func<T> func)
+        private static ActResult CatchExceptionAsync(Func<Task> action)
+        {
+            try
+            {
+                action.Invoke().GetAwaiter().GetResult();
+                return ActResult.Ok();
+            }
+            catch (Exception ex)
+            {
+                return ActResult.ThrewException(ex);
+            }
+        }
+
+        private static ActResult<T> CatchException<T>(Func<T> func)
         {
             T result;
 
             try
             {
                 result = func.Invoke();
+            }
+            catch (Exception ex)
+            {
+                return ActResult.ThrewException<T>(ex);
+            }
+
+            return ActResult.Ok(result);
+        }
+
+        private static ActResult<T> CatchExceptionAsync<T>(Func<Task<T>> func)
+        {
+            T result;
+
+            try
+            {
+                result = func.Invoke().GetAwaiter().GetResult();
             }
             catch (Exception ex)
             {
@@ -63,17 +107,44 @@ namespace TestLite
             new Acted(CatchException(actAction));
 
         /// <summary>
+        /// Define the <b>act</b>section of the test as an action
+        /// </summary>
+        public Acted ActAsync(Func<T, Task> actAction) =>
+            new Acted(CatchExceptionAsync(actAction));
+
+        /// <summary>
         /// Define the <b>act</b>section of the test as a function
         /// which returns the result of the test
         /// </summary>
         public Acted<TResult> Act<TResult>(Func<T, TResult> actFunc) =>
             new Acted<TResult>(CatchException(actFunc));
 
+        /// <summary>
+        /// Define the <b>act</b>section of the test as a function
+        /// which returns the result of the test
+        /// </summary>
+        public Acted<TResult> ActAsync<TResult>(Func<T, Task<TResult>> actFunc) =>
+            new Acted<TResult>(CatchExceptionAsync(actFunc));
+
+
         private ActResult CatchException(Action<T> func)
         {
             try
             {
                 func.Invoke(_underTest);
+                return ActResult.Ok();
+            }
+            catch (Exception ex)
+            {
+                return ActResult.ThrewException(ex);
+            }
+        }
+
+        private ActResult CatchExceptionAsync(Func<T, Task> func)
+        {
+            try
+            {
+                func.Invoke(_underTest).GetAwaiter().GetResult();
                 return ActResult.Ok();
             }
             catch (Exception ex)
@@ -89,6 +160,22 @@ namespace TestLite
             try
             {
                 result = func.Invoke(_underTest);
+            }
+            catch (Exception ex)
+            {
+                return ActResult.ThrewException<TOut>(ex);
+            }
+
+            return ActResult.Ok(result);
+        }
+
+        private ActResult<TOut> CatchExceptionAsync<TOut>(Func<T, Task<TOut>> func)
+        {
+            TOut result;
+
+            try
+            {
+                result = func.Invoke(_underTest).GetAwaiter().GetResult();
             }
             catch (Exception ex)
             {
@@ -117,17 +204,44 @@ namespace TestLite
             new Acted(CatchException(actAction));
 
         /// <summary>
+        /// Define the <b>act</b>section of the test as an action
+        /// </summary>
+        public Acted ActAsync(Func<T, TParam, Task> actAction) =>
+            new Acted(CatchExceptionAsync(actAction));
+
+        /// <summary>
         /// Define the <b>act</b>section of the test as a function
         /// which returns the result of the test
         /// </summary>
         public Acted<TResult> Act<TResult>(Func<T, TParam, TResult> actFunc) =>
             new Acted<TResult>(CatchException(actFunc));
 
+        /// <summary>
+        /// Define the <b>act</b>section of the test as a function
+        /// which returns the result of the test
+        /// </summary>
+        public Acted<TResult> ActAsync<TResult>(Func<T, TParam, Task<TResult>> actFunc) =>
+            new Acted<TResult>(CatchExceptionAsync(actFunc));
+
+
         private ActResult CatchException(Action<T, TParam> action)
         {
             try
             {
                 action.Invoke(_underTest, _parameter);
+                return ActResult.Ok();
+            }
+            catch (Exception ex)
+            {
+                return ActResult.ThrewException(ex);
+            }
+        }
+
+        private ActResult CatchExceptionAsync(Func<T, TParam, Task> action)
+        {
+            try
+            {
+                action.Invoke(_underTest, _parameter).GetAwaiter().GetResult();
                 return ActResult.Ok();
             }
             catch (Exception ex)
@@ -143,6 +257,22 @@ namespace TestLite
             try
             {
                 result = func.Invoke(_underTest, _parameter);
+            }
+            catch (Exception ex)
+            {
+                return ActResult.ThrewException<TOut>(ex);
+            }
+
+            return ActResult.Ok(result);
+        }
+
+        private ActResult<TOut> CatchExceptionAsync<TOut>(Func<T, TParam, Task<TOut>> func)
+        {
+            TOut result;
+
+            try
+            {
+                result = func.Invoke(_underTest, _parameter).GetAwaiter().GetResult();
             }
             catch (Exception ex)
             {
@@ -173,17 +303,48 @@ namespace TestLite
             new Acted(CatchException(actAction));
 
         /// <summary>
+        /// Define the <b>act</b>section of the test as an action
+        /// </summary>
+        public Acted ActAsync(Func<T, TParam1, TParam2, Task> actAction) =>
+            new Acted(CatchExceptionAsync(actAction));
+
+        /// <summary>
         /// Define the <b>act</b>section of the test as a function
         /// which returns the result of the test
         /// </summary>
         public Acted<TResult> Act<TResult>(Func<T, TParam1, TParam2, TResult> actFunc) =>
             new Acted<TResult>(CatchException(actFunc));
 
+        /// <summary>
+        /// Define the <b>act</b>section of the test as a function
+        /// which returns the result of the test
+        /// </summary>
+        public Acted<TResult> ActAsync<TResult>(Func<T, TParam1, TParam2, Task<TResult>> actFunc) =>
+            new Acted<TResult>(CatchExceptionAsync(actFunc));
+
+
         private ActResult CatchException(Action<T, TParam1, TParam2> action)
         {
             try
             {
                 action.Invoke(_underTest, _parameter1, _parameter2);
+                return ActResult.Ok();
+            }
+            catch (Exception ex)
+            {
+                return ActResult.ThrewException(ex);
+            }
+        }
+
+        private ActResult CatchExceptionAsync(Func<T, TParam1, TParam2, Task> action)
+        {
+            try
+            {
+                action
+                    .Invoke(_underTest, _parameter1, _parameter2)
+                    .GetAwaiter()
+                    .GetResult();
+
                 return ActResult.Ok();
             }
             catch (Exception ex)
@@ -199,6 +360,25 @@ namespace TestLite
             try
             {
                 result = func.Invoke(_underTest, _parameter1, _parameter2);
+            }
+            catch (Exception ex)
+            {
+                return ActResult.ThrewException<TOut>(ex);
+            }
+
+            return ActResult.Ok(result);
+        }
+
+        private ActResult<TOut> CatchExceptionAsync<TOut>(Func<T, TParam1, TParam2, Task<TOut>> func)
+        {
+            TOut result;
+
+            try
+            {
+                result = func
+                    .Invoke(_underTest, _parameter1, _parameter2)
+                    .GetAwaiter()
+                    .GetResult();
             }
             catch (Exception ex)
             {
--- TestLite/Test.cs
+++ TestLite/Test.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 [assembly: InternalsVisibleTo("TestLite.Tests")]
 
@@ -28,7 +29,17 @@ namespace TestLite
 
         /// <summary>
         /// Define the <b>arrange</b> section of the test as a function
-        /// which returns the unit under test and an additional paramater
+        /// which returns the unit under test
+        /// </summary>
+        public static Arranged<T> ArrangeAsync<T>(Func<Task<T>> arrangeFunc)
+        {
+            var underTest = arrangeFunc.Invoke().GetAwaiter().GetResult();
+            return new Arranged<T>(underTest);
+        }
+
+        /// <summary>
+        /// Define the <b>arrange</b> section of the test as a function
+        /// which returns the unit under test and an additional parameter
         /// </summary>
         public static Arranged<T, TParameter> Arrange<T, TParameter>(Func<(T, TParameter)> arrangeFunc)
         {
@@ -38,12 +49,33 @@ namespace TestLite
 
         /// <summary>
         /// Define the <b>arrange</b> section of the test as a function
-        /// which returns the unit under test and two additional paramaters
+        /// which returns the unit under test and an additional parameter
+        /// </summary>
+        public static Arranged<T, TParameter> ArrangeAsync<T, TParameter>(Func<Task<(T, TParameter)>> arrangeFunc)
+        {
+            var (underTest, parameter) = arrangeFunc.Invoke().GetAwaiter().GetResult();
+            return new Arranged<T, TParameter>(underTest, parameter);
+        }
+
+        /// <summary>
+        /// Define the <b>arrange</b> section of the test as a function
+        /// which returns the unit under test and two additional parameters
         /// </summary>
         public static Arranged<T, TParameter1, TParameter2> Arrange<T, TParameter1, TParameter2>(
             Func<(T, TParameter1, TParameter2)> arrangeFunc)
         {
             var (underTest, parameter1, parameter2) = arrangeFunc.Invoke();
+            return new Arranged<T, TParameter1, TParameter2>(underTest, parameter1, parameter2);
+        }
+        
+        /// <summary>
+        /// Define the <b>arrange</b> section of the test as a function
+        /// which returns the unit under test and two additional parameters
+        /// </summary>
+        public static Arranged<T, TParameter1, TParameter2> ArrangeAsync<T, TParameter1, TParameter2>(
+            Func<Task<(T, TParameter1, TParameter2)>> arrangeFunc)
+        {
+            var (underTest, parameter1, parameter2) = arrangeFunc.Invoke().GetAwaiter().GetResult();
             return new Arranged<T, TParameter1, TParameter2>(underTest, parameter1, parameter2);
         }
     }
